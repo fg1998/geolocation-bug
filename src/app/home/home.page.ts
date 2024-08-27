3805f3460e3dd01627bb64c7fd05d80fcb2dbe15
Fernando Garcia <fg1998@gmail.com>
2024-08-27 11:45:07 -0300

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, NgFor],
 })
 export class HomePage {
+  log: string[] = [];
   constructor() {}
+
+  async get() {
+    try {
+      this.log = [];
+      this.log.push('Init');
+      const check = await Geolocation.checkPermissions();
+      this.log.push(`Permissions : ${JSON.stringify(check)}`);
+      const resp = await Geolocation.getCurrentPosition();
+      this.log.push(`* - ${JSON.stringify(resp)}`);
+    } catch (err:any) {
+      this.log.push(`error : ${JSON.stringify(err)} `);
+    }
+  }
 }
